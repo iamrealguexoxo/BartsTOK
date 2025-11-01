3d59dfc22a5a98ed32124f86add00f63281a1641
iamrealguexoxo <guenther.koetschler@iu-study.org>
2025-11-01 02:17:31 +0100

    Update AssemblyInfo with new informational version and cache hash

--- obj/Debug/net9.0-windows/Barts Tok.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Barts Tok.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barts Tok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c18e86995efb3f47db06f393f9884cd9ce9c63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b006cc256bfcb2e494f87cf367d8a0387c8486")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barts Tok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barts Tok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
